920d8e2235da551b2459d16bd121b3d4e19f4b03
yassine.mazouz <yassine.mazouz@grupoavalon.es>
2023-06-14 15:36:56 +0100

    first commit

--- organization-service/src/main/java/com/microservices/organization/feign/UserFeign.java
+++ organization-service/src/main/java/com/microservices/organization/feign/UserFeign.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-@FeignClient(name = "userFeign", url = "https://microservices-springboot.herokuapp.com/user")
+@FeignClient(name = "userFeign", url = "http://localhost:8081")
 public interface UserFeign {
     @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
     void postUsers(@RequestBody User user);
